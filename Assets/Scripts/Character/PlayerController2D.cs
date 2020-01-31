8c593284dbdea3242d0c135e5299e716fd384403
Ajirogg <raimundo.guillaume@gmail.com>
2020-01-31 10:32:06 +0100

    Modify Camera comportment + creation worst case scenario

--- Assets/Scripts/Character/PlayerController2D.cs
+++ Assets/Scripts/Character/PlayerController2D.cs
@@ -20,6 +20,18 @@ public class PlayerController2D : MonoBehaviour
 
     bool isJumping;
 
+    private bool lookRight = false;
+
+    private Vector3 camTarget = Vector3.zero;
+    //[SerializeField] private float WaitTimeBeforeLookUp = 2.0f;
+    [SerializeField] private Vector3 camLookRight = Vector3.zero;
+    [SerializeField] private Vector3 camLookLeft = Vector3.zero;
+   // [SerializeField] private Vector3 camLookUp = Vector3.zero;
+
+    [Header("Scene Objects")]
+    [SerializeField] Camera cam;
+    [SerializeField] private GameObject steadyStick = null;
+    [SerializeField] private GameObject body = null;
 
     private CharacterController charController;
 
@@ -37,7 +49,6 @@ public class PlayerController2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         PlayerMovement();
     }
 
@@ -64,6 +75,7 @@ public class PlayerController2D : MonoBehaviour
 
 
         jumpInput();
+        CameraMovement();
     }
 
     //Detect if player is on a slope
@@ -110,81 +122,125 @@ public class PlayerController2D : MonoBehaviour
     }
 
 
-
-
-
-
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerController2D : MonoBehaviour
-{
-    // CHARACTER CONTROLLER
-    private CharacterController charController;
-    private Vector3 velocity;
-    private bool isGrounded = true;
-
-    [Header("MOVEMENT")]
-    public float playerSpeed = 7f;
-    public LayerMask layerGround;
-
-    [Header("JUMP")]
-    public float gravityForce = 10f;
-    public float jumpHeight = 2f;
-
-    // GROUND DETECTION PARAMETER
-    public float sphereRadius = 0.2f;
-
-    // Use this for initialization
-    void Start()
+    //Smooth travelling when the player turn arround
+    private void CameraMovement()
     {
-        charController = GetComponent<CharacterController>();
+        if ((Input.GetAxis("Horizontal") < 0 && lookRight) || (Input.GetAxis("Horizontal") > 0 && !lookRight))
+        {
+            lookRight = !lookRight;
+
+            if (lookRight)
+            {
+                camTarget = camLookRight;
+                body.transform.localRotation = new Quaternion(0, 0, 0, 0);
+                Debug.Log(body.transform.localRotation);
+            }
+            else
+            {
+                camTarget = camLookLeft;
+                body.transform.localRotation = new Quaternion(0, 180, 0, 0);
+                Debug.Log(body.transform.localRotation);
+            }
+        }
+        /*  if (lookUp)
+          {
+              if (cam.transform.localPosition != camTarget + camLookUp)
+              {
+                  if (!charController.isGrounded)
+                      cam.transform.localPosition = Vector3.Lerp(cam.transform.localPosition, camTarget + camLookUp, 0.025f);
+                  else
+                      cam.transform.localPosition = Vector3.Lerp(cam.transform.localPosition, camTarget + camLookUp, 0.1f);
+              }
+          }
+          else
+          {*/
+              if (cam.transform.localPosition != camTarget)
+              {
+                  if (!charController.isGrounded)
+                      cam.transform.localPosition = Vector3.Lerp(cam.transform.localPosition, camTarget, 0.025f);
+                  else
+                      cam.transform.localPosition = Vector3.Lerp(cam.transform.localPosition, camTarget, 0.05f);
+              }
+         // }
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+
+
+
+    /*using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class PlayerController2D : MonoBehaviour
     {
-        //GRAVITY DECLARED FIRST
-        velocity.y -= gravityForce * Time.deltaTime;
-        charController.Move(velocity * Time.deltaTime);
+        // CHARACTER CONTROLLER
+        private CharacterController charController;
+        private Vector3 velocity;
+        private bool isGrounded = true;
 
-        // VARS
-        float moveX = Input.GetAxis("Horizontal");
+        [Header("MOVEMENT")]
+        public float playerSpeed = 7f;
+        public LayerMask layerGround;
 
-        // MOVEMENT
-        Movement(moveX);
-     
-        // CUSTOM GROUND CHECK
-        isGrounded = Physics.CheckSphere(transform.position - new Vector3(0f, 1, 0f), sphereRadius, layerGround);
+        [Header("JUMP")]
+        public float gravityForce = 10f;
+        public float jumpHeight = 2f;
 
-        // Keep the following, jump won't work otherwise
-        if (isGrounded)
-            velocity.y = 0f;
+        // GROUND DETECTION PARAMETER
+        public float sphereRadius = 0.2f;
 
-        //Debug.Log(isGrounded); //Debug
+        // Use this for initialization
+        void Start()
+        {
+            charController = GetComponent<CharacterController>();
+        }
 
-        // JUMP
-        if (Input.GetButtonDown("Jump") && isGrounded)
-            velocity.y += Mathf.Sqrt(jumpHeight * 2f * gravityForce);
-        // Mathf.Sqrt useful when you want to have more control over the jump action.
+        // Update is called once per frame
+        void Update()
+        {
+            //GRAVITY DECLARED FIRST
+            velocity.y -= gravityForce * Time.deltaTime;
+            charController.Move(velocity * Time.deltaTime);
 
-        //Debug.Log(Mathf.Sqrt(jumpHeight * 2f * gravityForce)); //Debug
-    }
+            // VARS
+            float moveX = Input.GetAxis("Horizontal");
 
-    private void Movement(float moveX)
-    {
-        Vector3 move = new Vector3(moveX, 0, 0);
-        charController.Move(move * Time.deltaTime * playerSpeed);
-    }
-    */
+            // MOVEMENT
+            Movement(moveX);
+
+            // CUSTOM GROUND CHECK
+            isGrounded = Physics.CheckSphere(transform.position - new Vector3(0f, 1, 0f), sphereRadius, layerGround);
+
+            // Keep the following, jump won't work otherwise
+            if (isGrounded)
+                velocity.y = 0f;
+
+            //Debug.Log(isGrounded); //Debug
+
+            // JUMP
+            if (Input.GetButtonDown("Jump") && isGrounded)
+                velocity.y += Mathf.Sqrt(jumpHeight * 2f * gravityForce);
+            // Mathf.Sqrt useful when you want to have more control over the jump action.
+
+            //Debug.Log(Mathf.Sqrt(jumpHeight * 2f * gravityForce)); //Debug
+        }
+
+        private void Movement(float moveX)
+        {
+            Vector3 move = new Vector3(moveX, 0, 0);
+            charController.Move(move * Time.deltaTime * playerSpeed);
+        }
+        */
 
 
 
-/*private void OnDrawGizmos() //Sphere preview
-    {
-        Gizmos.color = Color.red;
+    /*private void OnDrawGizmos() //Sphere preview
+        {
+            Gizmos.color = Color.red;
 
-        Gizmos.DrawWireSphere(transform.position - new Vector3(0f, 1, 0f), sphereRadius);
-    }*/
+            Gizmos.DrawWireSphere(transform.position - new Vector3(0f, 1, 0f), sphereRadius);
+        }*/
 }
